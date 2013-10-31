829a3057694799bacf0d5f0b4c3fa0e2a03ef086
Max Boll <max@opoloo.de>
2013-10-31 16:42:13 +0100

    JS Tests

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
   // Calculate width of right column
-  if ($(window).width() > 1668) {
-    var width = $(window).width() - 500;
-    $('.right-col').css("width", width + "px");
-  }
+  // if ($(window).width() > 1668) {
+  //   var width = $(window).width() - 500;
+  //   $('.right-col').css("width", width + "px");
+  // }
   $(window).resize(function() {
-    $if ($(window).width() > 1668) {
+    if ($(window).width() > 1668) {
       var width = $(window).width() - 500;
-      alert(width);
       $('.right-col').css("width", width + "px");
     }
   });
