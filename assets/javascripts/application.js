d692eddc71e308f54d7fbfd9549e08dd23c2adaa
MyXoToD <webmaster@myxotod.de>
2014-11-04 16:20:01 +0100

    Fixed some minor bugs

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -85,7 +85,21 @@ $(document).ready(function() {
 
   // Calculating age
   getAge();
+  getHappyDay();
 });
+
+function getHappyDay() {
+  d = new Date();
+  weekdays = new Array(7);
+  weekdays[0] = "Sunday";
+  weekdays[1] = "Monday";
+  weekdays[2] = "Tuesday";
+  weekdays[3] = "Wednesday";
+  weekdays[4] = "Thursday";
+  weekdays[5] = "Friday";
+  weekdays[6] = "Saturday";
+  $("footer .day").html(weekdays[d.getDay()]);
+}
 
 function getAge() {
   var birthday = new Date("02 22, 1993");
